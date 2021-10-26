88bc2b708627187928ce16ce0b3ef8dafd14fda0
tranducmanh1998 <ducmanh198@gmail.com>
2021-10-27 00:18:13 +0700

    lab1-4 update

--- androiddev2022/lab1-4/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ androiddev2022/lab1-4/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -3,6 +3,7 @@ package vn.edu.usth.usthweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -10,7 +11,7 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        ForecastFragment ff = ForecastFragment.newInstance(", ")
+        ForecastFragment ff = ForecastFragment.newInstance("","");
         // Add the fragment to the 'container' FrameLayout
         getSupportFragmentManager().beginTransaction().add(
                 R.id.container, ff).commit();
@@ -18,27 +19,26 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(tag "Weather", msg "onStart() called");
+        Log.i( "Weather","onStart() called");
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(tag "Weather", msg "onResume() called");
+        Log.i( "Weather","onResume() called");
     }
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(tag "Weather", msg "onPause() called");
+        Log.i( "Weather","onPause() called");
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(tag "Weather", msg "onStop() called");
+        Log.i( "Weather","onStop() called");
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(tag "Weather", msg "onDestroy() called");
+        Log.i( "Weather", "onDestroy() called");
         }
     }
-}
--- androiddev2022/lab1-4/app/src/main/java/vn/edu/usth/usthweather/WeatherFragment.java
+++ androiddev2022/lab1-4/app/src/main/java/vn/edu/usth/usthweather/WeatherFragment.java
@@ -7,16 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ForecastFragment#newInstance} factory method to
+ * Use the {@link WeatherFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ForecastFragment extends Fragment {
+public class WeatherFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +24,7 @@ public class ForecastFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ForecastFragment() {
+    public WeatherFragment() {
         // Required empty public constructor
     }
 
@@ -37,11 +34,11 @@ public class ForecastFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ForecastFragment.
+     * @return A new instance of fragment WeatherFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ForecastFragment newInstance(String param1, String param2) {
-        ForecastFragment fragment = new ForecastFragment();
+    public static WeatherFragment newInstance(String param1, String param2) {
+        WeatherFragment fragment = new WeatherFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,27 +59,6 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       //View v = inflater.inflate(R.layout.fragment_forecast, container, false);
-        LinearLayout ll= new LinearLayout(getActivity());
-        ll.setBackground (0x80FF0000);
-        ll.setOrientation(LinearLayout.VERTICAL);
-
-        TextView monday = new TextView(getActivity());
-        monday.setText (monday);
-        ImageView sunny = new ImageView(getActivity());
-        sunny.setImageResource(R.drawable.sunny);
-
-        ll.addView(monday);
-        ll.addView(sunny);
-
-        //-----------
-        TextView monday = new TextView(getActivity());
-        wednesday.setText(wednesday is gonna be a rainny day);
-        ImageView rainny = new ImageView(getActivity());
-        rainny.setImageResource(R.drawable.rainny);
-
-        ll.addView(wednesday);
-        ll.addView(rainny);
-        return ll;
+        return inflater.inflate(R.layout.fragment_weather, container, false);
     }
 }
